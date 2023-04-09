233826ee7ca9cd8ea6c000b02be6a0701f91817f
patelavtx <apatel@aviatrix.com>
2023-04-09 12:23:49 +0100

    first

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.1.0"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "avtx"
+    workspaces {
+      name = "iac1" 
+    }
+  }
+}
